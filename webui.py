5493804ad10b0969b87d6ec26da17f4514f922b9
Arturo Mendivil <60411196+artmen1516@users.noreply.github.com>
2022-09-29 16:45:32 -0700

    enable share by default
    enable share to be able to reach colab instance externally

--- webui.py
+++ webui.py
@@ -95,7 +95,7 @@ def webui():
     )
 
     demo.launch(
-        share=cmd_opts.share,
+        share=True,
         server_name="0.0.0.0" if cmd_opts.listen else None,
         server_port=cmd_opts.port,
         debug=cmd_opts.gradio_debug,
